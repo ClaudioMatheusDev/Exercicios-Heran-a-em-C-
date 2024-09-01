7d1f67af588eed5eb5b60ba5acf9d31f5260531c
Claudio Matheus <claudiomatheus055@gmail.com>
2024-09-01 17:03:48 -0300

    Iniciando a atividade

---
+++ 01/Atividade01/Atividade01/Funcionario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade01
+{
+    internal class Funcionario
+    {
+
+        public string Nome {  get; set; }
+        public double Salario { get; set; }
+
+
+        public Funcionario() { }
+
+        public Funcionario(string nome, int salario)
+        {
+            Nome = nome;
+            Salario = salario;
+        }
+
+
+        public void addAumento(double valor)
+        {
+            Salario += valor;
+       }
+
+        public virtual double GanhoAnual()
+        {
+            return Salario * 12;
+        }
+
+        public virtual void exibeDados()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Salario: {Salario}");
+        } 
+
+
+    }
+}
